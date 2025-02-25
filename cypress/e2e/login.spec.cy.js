10be096c31ef8697a855bfc83e6e64a0c17cb208
BrunoChagas <brunocsega@gmail.com>
2025-02-25 09:34:37 -0300

    Creating fixtures and improving consts

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,35 +1,48 @@
+import userData from '../fixtures/userData.json'
+
 describe('Orange HRM Tests', () => {
 
   const selectorsList = {
     usernameField: "[name='username']",
     passwordField: "[name='password']",
     buttonClick: '.oxd-button',
-    sectionTitleTopBar: '.oxd-text--h6',
+    dashboardGrid: '.orangehrm-dashboard-grid',
     alertFail: '.oxd-alert'
+  }
+
+  const userData = {
+    loginSuccess:{
+      username:'Admin',
+      password:'admin123'
+    },
+    loginFail: {
+      username:'Test',
+      password:'Test'
+    }
   }
 
   it('Login - Success', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
 // Using right credentials    
-    cy.get(selectorsList.usernameField).type('Admin') //correct user
-    cy.get(selectorsList.passwordField).type('admin123') //correct password
+    cy.get(selectorsList.usernameField).type(userData.loginSuccess.username) //correct user
+    cy.get(selectorsList.passwordField).type(userData.loginSuccess.password) //correct password
     cy.get(selectorsList.buttonClick).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index') //checking the website address
-    cy.get(selectorsList.sectionTitleTopBar).contains('Dashboard') //checking element on new page
+    cy.get(selectorsList.dashboardGrid) //checking element on new page
   })
 
   it('Login - Fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
 // Using wrong credentials on username only
-    cy.get(selectorsList.usernameField).type('Adimin') //incorrect user
-    cy.get(selectorsList.passwordField).type('admin123') //correct password
+    cy.get(selectorsList.usernameField).type(userData.loginFail.username) //incorrect user
+    cy.get(selectorsList.passwordField).type(userData.loginSuccess.password) //correct password
     cy.get(selectorsList.buttonClick).click()
     cy.get(selectorsList.alertFail) //checking message of fail
 
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
 //using wrong credentials on password only
-    cy.get(selectorsList.usernameField).type('Admin') //correct user
-    cy.get(selectorsList.passwordField).type('admin223') //incorrect password
+    cy.get(selectorsList.usernameField).type(userData.loginSuccess.username) //correct user
+    cy.get(selectorsList.passwordField).type(userData.loginFail.password) //incorrect password
     cy.get(selectorsList.buttonClick).click()
     cy.get(selectorsList.alertFail) //checking message of fail
   })  
